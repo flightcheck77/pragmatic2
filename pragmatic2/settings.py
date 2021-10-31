0f183562c12e5106baaf898f168cad424d26dbad
koreapilot <kimbarb@hotmail.com>
2021-10-31 13:10:00 +0900

    .env file created

--- pragmatic2/settings.py
+++ pragmatic2/settings.py
@@ -13,15 +13,30 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os, environ
 
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+# Set the project base directory
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Take environment variables from .env file
+environ.Env.read_env(
+    env_file=os.path.join(BASE_DIR, '.env')
+)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s%!o)1%co5fzyriok6e^ct)y9*a%w+dqy4le()62s)a^+b1+1_'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
