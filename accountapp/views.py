0740925326ea8fdc29a99ef3a8f30105ac5839cd
koreapilot <kimbarb@hotmail.com>
2021-11-04 12:18:47 +0900

    signup function by CreateView

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
 from accountapp.models import HelloWorld
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 
 # def hello_world(request):
@@ -15,6 +19,17 @@ def hello_world(request):
         new_hello_world.text = temp
         new_hello_world.save()
 
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
+        # hello_world_list = HelloWorld.objects.all()
+
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!!'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
+
