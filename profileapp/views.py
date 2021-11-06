be4191ddbc9b6f0333f1d07ca5fc4dfa251e1986
koreapilot <kimbarb@hotmail.com>
2021-11-06 17:14:22 +0900

    detail.html changed in with user or not  on profileapp

--- profileapp/views.py
+++ profileapp/views.py
@@ -5,13 +5,14 @@ from profileapp.models import Profile
 from profileapp.forms import ProfileCreationForm
 from django.utils.decorators import method_decorator
 from profileapp.decorators import profile_ownership_required
+from django.urls import reverse
 
 
 class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = "target_profile"
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    # success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form):
@@ -21,6 +22,9 @@ class ProfileCreateView(CreateView):
 
         return super().form_valid(form)
 
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
+
 
 @method_decorator(profile_ownership_required, 'get')
 @method_decorator(profile_ownership_required, 'post')
@@ -28,5 +32,8 @@ class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = "target_profile"
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    # success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
