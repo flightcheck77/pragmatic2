e07ade941be0514f45bbbccae9c9319207e3fc1d
koreapilot <kimbarb@hotmail.com>
2021-11-13 15:49:36 +0900

    commentapp completed

--- commentapp/models.py
+++ commentapp/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
 from articleapp.models import Article
+from django.contrib.auth.models import User
 
 
 class Comment(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.SET_NULL, null=True, related_name='comment')
-    writer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='comment')
+    article = models.ForeignKey(Article, on_delete=models.SET_NULL, related_name='comment', null=True)
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='comment', null=True)
     content = models.TextField(null=False)
     created_at = models.DateTimeField(auto_now=True)
 
--- commentapp/views.py
+++ commentapp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DeleteView
 from django.urls import reverse
 from commentapp.forms import CommentCreationForm
 from commentapp.models import Comment
 from articleapp.models import Article
+from django.utils.decorators import method_decorator
+from commentapp.decorators import comment_ownership_required
 
 
 class CommentCreateView(CreateView):
@@ -19,7 +21,16 @@ class CommentCreateView(CreateView):
 
         return super().form_valid(form)
 
-        def get_success_url(self):
-            return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
 
 
+@method_decorator(comment_ownership_required, 'get')
+@method_decorator(comment_ownership_required, 'post')
+class CommentDeleteView(DeleteView):
+    model = Comment
+    context_object_name = 'target_comment'
+    template_name = 'commentapp/delete.html'
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
