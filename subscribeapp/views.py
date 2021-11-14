6f2c1324f1ab2236cdf9c23bd6f08d4be3d63909
koreapilot <kimbarb@hotmail.com>
2021-11-14 17:47:28 +0900

    subscribeapp completed

--- subscribeapp/views.py
+++ subscribeapp/views.py
@@ -1,10 +1,11 @@
 from subscribeapp.models import Subscription
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, ListView
 from django.urls import reverse
 from django.shortcuts import get_object_or_404
 from projectapp.models import Project
+from articleapp.models import Article
 
 
 @method_decorator(login_required, 'get')
@@ -25,3 +26,16 @@ class SubscriptionView(RedirectView):
             Subscription(user=user, project=project).save()
 
         return super(SubscriptionView, self).get(request, *args, **kwargs)
+
+
+@method_decorator(login_required, 'get')
+class SubscriptionListView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'subscribeapp/list.html'
+    paginate_by = 5
+
+    def get_queryset(self):
+        projects = Subscription.objects.filter(user=self.request.user).values_list('project')
+        article_list = Article.objects.filter(project__in=projects)
+        return article_list
