fead2233e34b3a3f53e6f689e0b9fe285eb65b02
koreapilot <kimbarb@hotmail.com>
2021-11-14 12:27:27 +0900

    projectapp completed & articles displyed with PageInfo on accountapp

--- projectapp/views.py
+++ projectapp/views.py
@@ -4,6 +4,8 @@ from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
+from django.views.generic.list import MultipleObjectMixin
+from articleapp.models import Article
 
 
 @method_decorator(login_required, 'get')
@@ -17,10 +19,16 @@ class ProjectCreateView(CreateView):
         return reverse('projectapp:detail', kwargs={'pk': self.object.pk})
 
 
-class ProjectDetailView(DetailView):
+class ProjectDetailView(DetailView, MultipleObjectMixin):
     model = Project
     context_object_name = 'target_project'
     template_name = 'projectapp/detail.html'
+
+    paginate_by = 25
+
+    def get_context_data(self, **kwargs):
+        object_list = Article.objects.filter(project=self.get_object())
+        return super(ProjectDetailView, self).get_context_data(object_list=object_list, **kwargs)
 
 
 class ProjectListView(ListView):
