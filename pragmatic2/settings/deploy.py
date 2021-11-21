282bd47e6d5507e85b6d2740f3c2d9a4fb3e5f71
koreapilot <kimbarb@hotmail.com>
2021-11-21 19:33:43 +0900

    DEBUG = True

--- pragmatic2/settings/deploy.py
+++ pragmatic2/settings/deploy.py
@@ -22,7 +22,7 @@ environ.Env.read_env(
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['158.247.199.183']
 # ALLOWED_HOSTS = ['*']
