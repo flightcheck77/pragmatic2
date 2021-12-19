61bae0d948849e9dc2ff605335b65d92cdf6ad91
koreapilot <kimbarb@hotmail.com>
2021-12-19 11:03:32 +0900

    Projects style changed & admin btn added in header.html

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.9.0
 
 WORKDIR /home/
 
-RUN echo "testing9"
+RUN echo "testing8"
 
 RUN git clone https://github.com/flightcheck77/pragmatic2.git
 
--- projectapp/views.py
+++ projectapp/views.py
@@ -25,7 +25,7 @@ class ProjectDetailView(DetailView, MultipleObjectMixin):
     context_object_name = 'target_project'
     template_name = 'projectapp/detail.html'
 
-    paginate_by = 25
+    paginate_by = 20
 
     def get_context_data(self, **kwargs):
         project = self.object
@@ -45,4 +45,4 @@ class ProjectListView(ListView):
     model = Project
     context_object_name = 'project_list'
     template_name = 'projectapp/list.html'
-    paginate_by = 25
+    paginate_by = 20
